b2c7b6774abd1a4aeb8d618104de17c748214f2a
rohitkshirsagar19 <rohitkshirsagar1904@gmail.com>
2025-05-20 18:22:29 +0530

    Fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,21 +1,49 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig")
-    lspconfig.pylsp.setup({
-      settings = {
-        pylsp = {
-          plugins = {
-            pycodestyle = { enabled = false },
-            ruff = { enabled = true },
-            black = { enabled = true },
-            isort = { enabled = true },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      -- Setup Mason and Mason-LSPConfig
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright", "html", "cssls", "tsserver" }, -- Use pyright instead of pylsp
+        automatic_installation = true,
+      })
+
+      local lspconfig = require("lspconfig")
+
+      -- On-attach function for keybindings
+      local on_attach = function(client, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to Definition" }))
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to Declaration" }))
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code Action" }))
+        vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, vim.tbl_extend("force", opts, { desc = "Format File" }))
+        vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename Symbol" }))
+        vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show Diagnostics" }))
+      end
+
+      -- Setup pyright
+      lspconfig.pyright.setup({
+        on_attach = on_attach,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace",
+            },
           },
         },
-      },
-    })
-    lspconfig.html.setup({})
-    lspconfig.cssls.setup({})
-    lspconfig.tsserver.setup({})
-  end,
+      })
+
+      -- Setup other LSP servers
+      lspconfig.html.setup({ on_attach = on_attach })
+      lspconfig.cssls.setup({ on_attach = on_attach })
+      lspconfig.tsserver.setup({ on_attach = on_attach })
+    end,
+  },
 }
